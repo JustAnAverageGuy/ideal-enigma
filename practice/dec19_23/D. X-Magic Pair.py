b2f7464df205acf4cdfb4c3e88dbce1df3861a68
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-25 03:41:14 +0530

    Update repo

---
+++ practice/dec19_23/D. X-Magic Pair.py
@@ -0,0 +1,36 @@
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀HELO⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+# ⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+# ⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+#
+# D. X-Magic Pair
+# 2000, 512
+# Friday 22 December 2023 09:37:44 PM
+#
+from math import gcd
+import sys
+
+input = sys.stdin.readline
+
+
+def resolv(a, b, x):
+    a, b = sorted([a, b])
+    if x in [a, b]: return True
+    if x > b: return False
+    if a == 0: return False
+    if x > a:
+        if (b - x) % a == 0: return True
+    return resolv(b % a, a, x)
+
+
+def solve():
+    a, b, x = map(int, input().strip().split())
+    if x % gcd(a, b) != 0: return False
+    return resolv(a, b, x)
+
+
+for _ in range(int(input())):
+    print("YES" if solve() else "NO")
