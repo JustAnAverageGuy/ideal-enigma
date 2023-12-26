a652466f738db8add7811f288ebb720010a89871
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-26 19:02:05 +0530

    Add problem ?

--- practice/dec19_23/A. Least Product.py
+++ practice/dec19_23/A. Least Product.py
@@ -1,3 +1,22 @@
+import sys;input=sys.stdin.readline
+
+def solve():
+    n = int(input())
+    A = list(map(int,input().strip().split()))
+    negs = 0
+    zer = 0
+    for i in range(n): 
+        if A[i] < 0: negs-=-1
+        if A[i] == 0: zer-=-1
+
+    if (negs)%2 == 1 or zer: print(0); return
+    print(1)
+    print(1, 0)
+
+for _ in range(int(input())): solve()
+
+
+
 # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 # ⠀HELO⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
 # ⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
@@ -6,7 +25,7 @@
 # ⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
 # ⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
 #
-# $(PROBLEM)
-# $(TIMELIM), $(MEMLIM)
-# $(DATE)
+# A. Least Product
+# 1000, 256
+# Monday 25 December 2023 04:31:03 AM
 #
