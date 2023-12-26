5a4d71a7b354a98c3b88b7374ae6423f2b69dd3d
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-27 02:06:52 +0530

    Test submitter extension

---
+++ practice/dec19_23/A. Cut Ribbon.py
@@ -0,0 +1,45 @@
+n,a,b,c = map(int,input().strip().split())
+
+m = float('-inf')
+for x in range(0, (n+a-1)//a + 1):
+    for y in range(0, (n+b-1)//b + 1):
+        cz = n - a*x - b*y
+        if cz <= 0 or cz%c !=0: continue
+        z = cz//c
+        m = max(m ,x+y+z)
+
+print(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀HELO⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+# ⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+# ⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+#
+# A. Cut Ribbon
+# 1000, 256
+#
+# https://codeforces.com/problemset/problem/189/A
+# Wednesday 27 December 2023 01:42:09 AM
+#
---
+++ practice/dec19_23/B. Bets.py
@@ -0,0 +1,49 @@
+n,m = map(int,input().strip().split())
+
+players = []
+for i in range(m):
+    l,r,t,c = map(int,input().strip().split())
+    players.append((i,l,r,t,c))
+
+profit = 0
+
+for section in range(1,n+1):
+    runners = [(i[3],i[0],i[4]) for i in players if i[2]>=section>=i[1]]
+    if runners:
+        winner = min(runners)
+        profit += winner[-1]
+print(profit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀HELO⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+# ⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+# ⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+#
+# B. Bets
+# 2000, 256
+#
+# https://codeforces.com/problemset/problem/69/B
+# Tuesday 26 December 2023 07:14:07 PM
+#
