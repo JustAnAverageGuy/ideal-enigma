d490befc8304509dbf98de26a10f2cc9a81e1c62
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-01-05 16:03:22 +0530

    backup

---
+++ practice/dec_24/A__Divisibility_Problem.py
@@ -0,0 +1,22 @@
+import sys;input=sys.stdin.readline
+
+def solve():
+    a,b = map(int,input().strip().split())
+    print((-a)%b)
+    
+
+for _ in range(int(input())): solve()
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀HELO⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+# ⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+# ⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+# ⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+#
+# A. Divisibility Problem
+# 1000, 256
+#
+# https://codeforces.com/problemset/problem/1328/A
+# Sunday 05 January 2025 15:57:11 +0530
+#
